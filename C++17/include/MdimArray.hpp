39f2c667baf08bd0e5c387f165577935b393ce43
wme7 <manuel.ade@gmail.com>
2022-02-10 16:15:20 +0100

    Initial commit

---
+++ C++17/include/MdimArray.hpp
@@ -0,0 +1,223 @@
+#ifndef MULTI_DIMENSIONAL_ARRAY_HPP
+#define MULTI_DIMENSIONAL_ARRAY_HPP
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+
+/*
+ * Multidimensionnal Array in C++ 
+ * 
+ * Original code by RM 
+ * Modifications by MD
+ * 
+ * T: typename (int/size_t/float/double)
+ * D: number of dimensions
+ */
+
+template<typename T, size_t D>
+class MArray {
+
+protected:
+    T*   m_data;
+    size_t m_total_size;
+    size_t m_D;
+    bool m_owned;
+    bool m_allocated;
+    std::vector<size_t> m_dims; // real dimensions
+
+public:
+    // Constructors & Destructor
+    MArray():m_data(nullptr), m_total_size(0), m_D(D), m_owned(false), m_allocated(false) {}
+    MArray(MArray const& Another)
+    {
+        //std::cout << "constructor by copy " << std::endl;
+        m_total_size = Another.m_total_size;
+        m_D = Another.m_D;
+        m_dims = Another.m_dims;
+        m_data = new T[m_total_size];
+        // copy of the values from A to current array
+        for(size_t i=0; i<m_total_size; i++)
+        {
+            m_data[i] = Another.m_data[i];
+        }
+        m_owned = true;
+        m_allocated = true;
+    }
+    MArray(std::vector<size_t> const& dim)
+    {
+        //std::cout << "constructor by dim" << std::endl;
+        m_D = D;
+        m_total_size=1;
+        for(size_t i=0; i<m_D; i++)
+        {
+           m_total_size *= dim[i];
+           m_dims.push_back(dim[i]);
+           //std::cout << "dim" << dim[i] << std::endl;
+        }
+        //std::cout << "total size :" << std::endl;
+        //std::cout << m_total_size << std::endl;
+        m_data = new T[m_total_size];
+        m_allocated = true;
+        m_owned = true;
+    }
+    MArray(std::vector<size_t> const& dim, T const val)
+    {
+        //std::cout << "constructor by dim with constant value" << std::endl;
+        m_D = D;
+        m_total_size=1;
+        allocate(dim);
+        for(size_t i=0;i<m_total_size;i++)
+            {m_data[i] = val;}
+    }
+    ~MArray()
+    {
+        if (m_allocated==true && m_owned==true)
+        {
+            delete [] m_data;
+            m_data = nullptr;
+        }
+        else
+        {
+            //std::cout<<"not allocated !" <<std::endl; 
+        }
+    };
+
+    // Operator overloading
+    MArray& operator=(MArray other)
+    {
+        m_total_size = other.m_total_size;
+        m_D = other.m_D;
+        m_dims = other.m_dims;
+        m_data = new T[m_total_size];
+        // copy of the values from A to current array
+        for(size_t i=0; i<m_total_size; i++)
+        {
+            m_data[i] = other.m_data[i];
+        }
+        m_owned = true;
+        m_allocated = true;
+        return *this;
+    }
+
+    T& operator()(uint i) { return m_data[i]; }
+    const T& operator()(uint i) const { return m_data[i]; }
+    T& operator()(uint i, uint j) { return m_data[i*m_dims[1]+j]; }
+    const T& operator()(uint i, uint j) const {return m_data[i*m_dims[1]+j]; }
+    T& operator()(uint i, uint j, uint k) { return m_data[ i*m_dims[1]*m_dims[2]+j*m_dims[2]+k]; }
+    const T& operator()(uint i, uint j, uint k) const {return m_data[i*m_dims[1]*m_dims[2]+j*m_dims[2]+k]; }
+
+    // Members functions that modify the data in the object
+    void allocate(std::vector<size_t>const& dims2);
+    void set_value(T val);    
+
+    // Member functions that DO NOT modify the data in the object
+    std::vector<size_t> dims() const{ return m_dims; }
+    size_t total_size() const{ return m_total_size; }
+    size_t N() const{ return m_D; }
+    void print() const;
+    T sum() const;
+    T max() const;
+    T min() const;
+    T amax() const;
+    T* data() const { return m_data; }
+};
+
+template<typename T, size_t D>
+void MArray<T,D>::allocate(std::vector<size_t> const &dims2)
+{
+    m_dims = dims2;
+    m_total_size = 1;
+    for (auto element : dims2){ m_total_size *= element;}
+    m_data = new T[m_total_size];
+    m_allocated = true;
+    m_owned = true;
+}
+
+template<typename T, size_t D>
+void MArray<T,D>::set_value(T val)
+{
+    for (size_t i=0; i<m_total_size; i++) { m_data[i]=val; }
+}
+
+template<typename T, size_t D>
+void MArray<T,D>::print() const
+{
+    if (m_allocated==1)
+    {
+        // Set print-out precision for 2D & 3D arrays
+        std::cout.precision(4); // I need to check up to 8 decimals
+
+        std::cout<<D<<"Darray:\n"<<std::endl;
+        if(D==1)
+        {
+            // i-elements are printed as columns 
+            for(size_t i=0; i<m_total_size; i++){
+                std::cout << m_data[i] << std::endl;
+            }
+        }
+        if(D==2 && m_dims.size()==2)
+        {
+            // i-elements are printed as columns 
+            // j-elements are printed as rows
+            for(size_t i=0; i<m_dims[0]; i++){ 
+                for(size_t j=0; j<m_dims[1]; j++){
+                    std::cout << m_data[i * m_dims[1] + j] << " " << std::scientific;
+                } std::cout << std::endl;
+            } std::cout << std::endl;
+        }
+        if(D==3 && m_dims.size()==3)
+        {
+            // i-elements are printed as columns 
+            // j-elements are printed as rows
+            // k-elements are printed as ij-arrays
+            for(size_t k=0; k<m_dims[2]; k++){
+                for(size_t i=0; i<m_dims[0]; i++){
+                    for(size_t j=0; j<m_dims[1]; j++){
+                        std::cout << m_data[i * m_dims[1]*m_dims[2] + j * m_dims[2] + k] << " " << std::scientific;
+                    } std::cout << std::endl;
+                } std::cout << std::endl;
+            } std::cout << std::endl;
+        }
+    }
+    else
+    {
+        std::cout<<"Marray not allocated\n" <<std::endl;   
+    }
+}
+
+template<typename T, size_t D>
+T MArray<T,D>::sum() const
+{   T s=0;
+    for (size_t i=0; i<m_total_size; i++) { s=s+m_data[i]; }
+    return s;
+}
+
+template<typename T, size_t D>
+T MArray<T,D>::max() const 
+{   T s=-1e10;
+    for (size_t i=0; i<m_total_size; i++) { s=std::max(m_data[i],s); }
+    return s;
+}
+
+template<typename T, size_t D>
+T MArray<T,D>::amax() const
+{   T s=-1e10,s1;
+    for (size_t i=0; i<m_total_size; i++) 
+    {   s1 = std::abs(m_data[i]);
+        s = std::max(s1,s); 
+    }
+    return s;
+}
+
+template<typename T, size_t D>
+T MArray<T,D>::min() const
+{   T s=1e10;
+    for (size_t i=0; i<m_total_size; i++) { s=std::min(m_data[i],s); }
+    return s;
+}
+
+#endif
---
+++ C++17/include/Tetrahedron.hpp
@@ -0,0 +1,103 @@
+#ifndef TETRAHEDRON
+#define TETRAHEDRON
+
+#include <iostream>
+#include "Node.hpp"
+
+class Tetrahedron{
+    /*
+    ================================================================
+    % ----------------- %
+    % Tetrahedron class %
+    % ----------------- %
+        Describes an element by its four vertex ids, the global 
+        identification number (id) and the element type.
+
+    Impelmentation: MD
+    ---------------  
+    Input:
+        ------
+        Id (size_t) -> global indentification number,
+        N1 (Node) -> first vertex id,
+        N2 (Node) -> second vertex id
+        N3 (Node) -> third vertex id,
+        N4 (Node) -> fourth vertex id,
+        ElementType (integer) -> element type.
+        Partition (size_t) -> partition id
+    ================================================================       
+    */
+
+private:
+    size_t Id;
+    Node<double> N1;
+    Node<double> N2;
+    Node<double> N3;
+    Node<double> N4;
+    int ElementType;
+    size_t Partition;
+    double Volume;
+
+public:
+    Tetrahedron(): Id(0), ElementType(-1), Partition(0), Volume(0.){}
+    Tetrahedron(size_t Id_in, Node<double> N1_in, Node<double> N2_in, Node<double> N3_in, Node<double> N4_in, int ElementType_in, size_t Partition_in)
+    {
+        Id = Id_in;
+        N1 = N1_in;
+        N2 = N2_in;
+        N3 = N3_in;
+        N4 = N4_in;
+        ElementType = ElementType_in;
+        Partition = Partition_in;
+        set_Volume();
+    }
+    // Member function that do modify the object
+    void set_Volume() 
+    {   // Dx = (cy-ay).*(dz-az) - (cz-az).*(dy-ay);           ax = N1.get_x;  bx = N2.get_x;  cx = N3.get_x;  dx = N4.get_x;
+        // Dy = (cz-az).*(dx-ax) - (cx-ax).*(dz-az);   where:  ay = N1.get_y;  by = N2.get_y;  cy = N3.get_y;  dy = N4.get_y;
+        // Dz = (cx-ax).*(dy-ay) - (cy-ay).*(dx-ax);           az = N1.get_z;  by = N2.get_z;  cz = N3.get_z;  dz = N4.get_z;
+        // so that: Volume = ((bx-ax).*Dx + (by-ay).*Dy + (bz-az).*Dz) / 6;
+        double Dx = (N3.get_y()-N1.get_y())*(N4.get_z()-N1.get_z()) - (N3.get_z()-N1.get_z())*(N4.get_y()-N1.get_y());
+        double Dy = (N3.get_z()-N1.get_z())*(N4.get_x()-N1.get_x()) - (N3.get_x()-N1.get_x())*(N4.get_z()-N1.get_z());
+        double Dz = (N3.get_x()-N1.get_x())*(N4.get_y()-N1.get_y()) - (N3.get_y()-N1.get_y())*(N4.get_x()-N1.get_x());
+        Volume = 0.166666666666667*((N2.get_x()-N1.get_x())*Dx + (N2.get_y()-N1.get_y())*Dy + (N2.get_z()-N1.get_z())*Dz);
+    }
+    double testNodeOrder(const bool print)
+    {   // If Volume is negative, re-order the nodes inside the element
+        if (Volume<0)
+        {   // SWAP nodes: EToV(i,:) = EToV(i,[1 2 4 3]);
+            Node<double> Nt=N3; N3=N4; N4=Nt;
+        }
+        set_Volume(); if(print) std::cout << "element("<< Id <<").Volume= " << Volume << std::endl;
+        return Volume;
+    }
+    // Member functions that do not modify the object
+    size_t get_Id() const {return Id;}
+    size_t get_N1Id() const {return N1.get_Id();}
+    size_t get_N2Id() const {return N2.get_Id();}
+    size_t get_N3Id() const {return N3.get_Id();}
+    size_t get_N4Id() const {return N4.get_Id();}
+    double get_N1x() const {return N1.get_x();}
+    double get_N1y() const {return N1.get_y();}
+    double get_N1z() const {return N1.get_z();}
+    double get_N2x() const {return N2.get_x();}
+    double get_N2y() const {return N2.get_y();}
+    double get_N2z() const {return N2.get_z();}
+    double get_N3x() const {return N3.get_x();}
+    double get_N3y() const {return N3.get_y();}
+    double get_N3z() const {return N3.get_z();}
+    double get_N4x() const {return N4.get_x();}
+    double get_N4y() const {return N4.get_y();}
+    double get_N4z() const {return N4.get_z();}
+    int get_ElementType() const {return ElementType;}
+    size_t get_Partition() const {return Partition;}
+    void print() const
+    {
+        std::cout << "Tetrahedron: " << Id << ", ElementType: " << ElementType << ", Partition: " << Partition << ", Volume: " << Volume << std::endl;
+        std::cout << "\t"; N1.print();
+        std::cout << "\t"; N2.print();
+        std::cout << "\t"; N3.print();
+        std::cout << "\t"; N4.print();
+    }
+};
+
+#endif
